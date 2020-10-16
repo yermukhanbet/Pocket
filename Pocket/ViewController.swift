b6844bd3749e7139ced9f4e91327d2a2ff51da27
Yessen Yermukhanbet <y.yesen.y@gmail.com>
2020-10-17 00:46:27 +0900

    Initial Commit

---
+++ Pocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pocket
+//
+//  Created by Yessen Yermukhanbet on 10/17/20.
+//  Copyright © 2020 Yessen Yermukhanbet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
